e916e6f1d13ba9612d7cd2221e7b17d4ebef85bb
Luis Echeverri <elileec@hotmail.com>
2025-07-20 01:00:15 -0500

    Creacion repositorio topicos

--- src/main/java/foroHub/foroHub/controller/TopicoController.java
+++ src/main/java/foroHub/foroHub/controller/TopicoController.java
@@ -1,7 +1,10 @@
 package foroHub.foroHub.controller;
 
 import foroHub.foroHub.topico.DatosRegistroTopico;
+import foroHub.foroHub.topico.Topico;
+import foroHub.foroHub.topico.TopicoRepository;
 import jakarta.servlet.ServletOutputStream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/topicos")
 public class TopicoController {
 
+    @Autowired
+    private TopicoRepository repository;
+
     @PostMapping
-    public void registrar(@RequestBody DatosRegistroTopico json) {
-        System.out.println(json);
+    public void registrar(@RequestBody DatosRegistroTopico datos) {
+        repository.save(new Topico(datos));
 
     }
 }
---
+++ src/main/java/foroHub/foroHub/topico/Topico.java
@@ -0,0 +1,36 @@
+package foroHub.foroHub.topico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private String fechaCreacion;
+    private String status;
+    private String autor;
+    private String curso;
+
+    public Topico(DatosRegistroTopico datos) {
+        this.id = null;
+        this.titulo = datos.titulo();
+        this.mensaje = datos.mensaje();
+        this.fechaCreacion = datos.fechaCreacion();
+        this.status = datos.status();
+        this.autor = datos.autor();
+        this.curso = datos.curso();
+    }
+}
