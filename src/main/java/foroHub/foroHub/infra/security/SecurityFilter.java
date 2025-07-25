7f614bec593635c0bf1fd838522433ca484c1558
Luis Echeverri <elileec@hotmail.com>
2025-07-24 22:53:23 -0500

    creando el filtro de seguridad

---
+++ src/main/java/foroHub/foroHub/infra/security/SecurityFilter.java
@@ -0,0 +1,20 @@
+package foroHub.foroHub.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.logging.Filter;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
