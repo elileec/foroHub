f1d6d30fb73c8d24ed48c3725fff38805d5abc9e
Luis Echeverri <elileec@hotmail.com>
2025-07-26 00:18:36 -0500

    configurar codigo del filtro antes que filtro de spring

--- src/main/java/foroHub/foroHub/infra/security/SecurityFilter.java
+++ src/main/java/foroHub/foroHub/infra/security/SecurityFilter.java
@@ -1,10 +1,13 @@
 package foroHub.foroHub.infra.security;
 
+import foroHub.foroHub.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,22 +18,35 @@ import java.util.logging.Filter;
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
+    private UsuarioRepository repository;
+
+    @Autowired
     private TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var tokenJWT = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
 
+        var tokenJWT = recuperarToken(request);
+        if(tokenJWT != null){
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            //el usuario está logueado
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+        }
 
         filterChain.doFilter(request, response); //seguir con la cadena de filtros
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader ==null){
-            throw new RuntimeException("Token JWT no enviado en el encabezado de Authorization");
+        if(authorizationHeader !=null){
+            return authorizationHeader.replace("Bearer ","");
         }
-        return authorizationHeader.replace("Bearer ","");
+        return null;
+
     }
 }
