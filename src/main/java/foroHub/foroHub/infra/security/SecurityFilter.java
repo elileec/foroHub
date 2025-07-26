de68888524d0014446f44cf3fae6551d61b82795
Luis Echeverri <elileec@hotmail.com>
2025-07-25 20:58:13 -0500

    Recuperando el token

--- src/main/java/foroHub/foroHub/infra/security/SecurityFilter.java
+++ src/main/java/foroHub/foroHub/infra/security/SecurityFilter.java
@@ -15,6 +15,16 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        filterChain.doFilter(request, response);
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
+        filterChain.doFilter(request, response); //seguir con la cadena de filtros
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader ==null){
+            throw new RuntimeException("Token JWT no enviado en el encabezado de Authorization");
+        }
+        return authorizationHeader;
     }
 }
